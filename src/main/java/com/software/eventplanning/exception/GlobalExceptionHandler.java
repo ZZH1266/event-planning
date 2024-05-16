7ff59b27828e7a59d856d75ba447954eba3d619f
Zangzhenghao <zzh18736680168@outlook.com>
2024-05-16 22:54:35 +0800

    后端：完善异常输出功能

--- src/main/java/com/software/eventplanning/exception/GlobalExceptionHandler.java
+++ src/main/java/com/software/eventplanning/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.software.eventplanning.exception;
 
 
+import com.software.eventplanning.common.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,9 +14,9 @@ public class GlobalExceptionHandler {
      * @return
      * @ExceptionHandler相当于controller的@RequestMapping 如果抛出的的是ServiceException，则调用该方法
      */
-//    @ExceptionHandler(ServiceException.class)
-//    @ResponseBody
-//    public Result handle(ServiceException se) {
-//        return Result.error(se.getCode(), se.getMessage());
-//    }
+    @ExceptionHandler(ServiceException.class)
+    @ResponseBody
+    public Result handle(ServiceException se) {
+        return Result.error(se.getCode(), se.getMessage());
+    }
 }
