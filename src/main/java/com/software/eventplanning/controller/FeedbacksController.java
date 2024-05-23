40511243e94be6141d07e68c15e11fcabbb80d6e
Gye166 <U202142013@xs.ustb.edu.cn>
2024-05-23 20:07:27 +0800

    活动反馈提交

---
+++ src/main/java/com/software/eventplanning/controller/FeedbacksController.java
@@ -0,0 +1,35 @@
+package com.software.eventplanning.controller;
+
+import com.software.eventplanning.common.Constants;
+import com.software.eventplanning.common.Result;
+import com.software.eventplanning.controller.dto.FeedbacksDTO;
+import com.software.eventplanning.controller.dto.RegisterDTO;
+import com.software.eventplanning.service.IFeedbacksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class FeedbacksController {
+
+    @Autowired
+     private IFeedbacksService feedbacksService;
+
+    @PostMapping("/feedbacksubmitted")
+    @ResponseBody
+    public Result submit(@RequestBody FeedbacksDTO feedbacksDTO) {
+        Integer rating = feedbacksDTO.getRating();
+        String context = feedbacksDTO.getContext();
+        feedbacksDTO.setUserId(1);
+        feedbacksDTO.setActivityId(1);
+        if (rating == null ||context == null) {
+            return Result.error(Constants.CODE_400,"参数错误");
+        }
+        return Result.success(feedbacksService.submitted(feedbacksDTO));
+    }
+
+}
---
+++ src/main/java/com/software/eventplanning/mapper/FeedbacksMapper.java
@@ -0,0 +1,7 @@
+package com.software.eventplanning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.software.eventplanning.entity.Feedbacks;
+
+public interface FeedbacksMapper extends BaseMapper<Feedbacks> {
+}
