69862d5c3b84509f05d3c3afc8265a3a4ad22c08
Zangzhenghao <zzh18736680168@outlook.com>
2024-05-28 12:54:18 +0800

    后端：注册时检查用户名和邮箱格式

--- src/main/java/com/software/eventplanning/controller/RegisterController.java
+++ src/main/java/com/software/eventplanning/controller/RegisterController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.regex.Pattern;
 
 @RestController
 public class RegisterController {
@@ -20,9 +21,12 @@ public class RegisterController {
 
     @PostMapping("/sendEmail")
     @ResponseBody
-    public String sendEmail(String email, HttpSession session) {
+    public Result sendEmail(String email, HttpSession session) {
+        if (!Pattern.matches("^(\\w+([-.][A-Za-z0-9]+)*){3,18}@\\w+([-.][A-Za-z0-9]+)*\\.\\w+([-.][A-Za-z0-9]+)*$", email)) {
+            return Result.error(Constants.CODE_400,"邮箱格式错误");
+        }
         emailService.sendEmail(email, session);
-        return "success";
+        return Result.success();
     }
 
     @PostMapping("/register")
@@ -32,6 +36,15 @@ public class RegisterController {
         String password = registerDTO.getPassword();
         String email = registerDTO.getEmail();
         String code = registerDTO.getCode();
+        //用户名正则，4到16位（字母，数字，下划线，减号）
+        if (!Pattern.matches("^[a-zA-Z0-9_-]{4,16}$", username)) {
+            return Result.error(Constants.CODE_400,"用户名格式错误");
+        }
+
+        if (!Pattern.matches("^(\\w+([-.][A-Za-z0-9]+)*){3,18}@\\w+([-.][A-Za-z0-9]+)*\\.\\w+([-.][A-Za-z0-9]+)*$", email)) {
+            return Result.error(Constants.CODE_400,"邮箱格式错误");
+        }
+
         registerDTO.setRole(0);
         if (username == null || password == null || email == null || code == null) {
             return Result.error(Constants.CODE_400,"参数错误");
